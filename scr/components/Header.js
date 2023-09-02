1adac2c02464b33ced98314df991600457fdcdd8
Digvijaysingh0707 <digvijaysinghonly1@gmail.com>
2023-09-02 15:27:23 +0530

    added link tags on header

--- scr/components/Header.js
+++ scr/components/Header.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import logo from '../../assets/images/logo.png';
+import { Link } from "react-router-dom";
 
 
 const Header = () => {
@@ -23,9 +24,15 @@ const NavComponent = () => {
   return (
     <div className="nav-items">
       <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
         <li>Cart</li>
 
         <button className="login"
